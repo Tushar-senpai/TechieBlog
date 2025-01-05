304a53d70a4da07c0af404844dda0f28d7f8fe25
Sumit <goraisumit01@gmail.com>
2025-01-05 11:02:01 +0530

    fix logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import logo from '../assets/logo1.jpg'
+import logo from '../assets/logo.png'
 
-function Logo({width = '100px'}) {
+function Logo({width=100}) {
   return (
     <div>
-      <img src={logo} alt="" />
+      <img src={logo} alt="" width={100} />
     </div>
   )
 }
