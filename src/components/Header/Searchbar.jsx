f7b45cfb291cf14a0a3a9c87173fecaac32eb5e5
soham247 <sohamsadhukhan247@gmail.com>
2025-01-08 15:22:33 +0530

    added mobile menu

--- src/components/Header/Searchbar.jsx
+++ src/components/Header/Searchbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import SearchIcon from '@mui/icons-material/Search';
 
 function Searchbar() {
     const navigate = useNavigate()
@@ -15,15 +16,15 @@ function Searchbar() {
         
     }
     return (
-        <form onSubmit={handlesubmit} className='flex items-center justify-center w-[full] md:w-auto'>
+        <form onSubmit={handlesubmit} className='flex items-center justify-center w-[full] md:w-auto border border-orange-400 rounded-lg'>
             <input 
             type="text" 
             name="search" 
             value={search}
             onChange={(e) => setSearch(e.target.value)}
             placeholder='Search blogs'
-            className='px-4 py-2 w-[70%] md:w-full bg-transparent border outline-none rounded-tl-md rounded-bl-md' />
-            <button className='border px-3 py-2 rounded-tr-md rounded-br-md' type='submit'>Search</button>
+            className='px-4 py-2 w-[70%] md:w-full bg-transparent outline-none' />
+            <button className='px-2 py-2 rounded-tr-md rounded-br-md' type='submit'><SearchIcon className='text-orange-500' fontSize='medium' /></button>
         </form>
     )
 }
