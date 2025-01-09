c40604c2eb525bff29bfb56e45b3f23ba7c6c325
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-01-09 12:52:12 +0530

    Added testimonial section

--- src/components/index.js
+++ src/components/index.js
@@ -14,6 +14,7 @@ import Input from './Input'
 import Select from './Select'
 import ForgotPassword from './ForgotPassword'
 import ResetPassword from './ResetPassword'
+import Testimonials from './Testimonials'
 
 export {
     Header,
@@ -31,5 +32,6 @@ export {
     Input,
     Select,
     ForgotPassword,
-    ResetPassword
+    ResetPassword,
+    Testimonials
 }
