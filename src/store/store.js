acd4692b126132bf1a73faa70c08316954aca77c
abhaykumar-0285 <abhay.kr2803@gmail.com>
2025-01-10 15:17:06 +0530

    removed one of them scroll button from website

--- src/store/store.js
+++ src/store/store.js
@@ -6,23 +6,5 @@ const store = configureStore({
        auth : authSlice,
     }
 });
-// Get the button element
-const scrollToTopButton = document.getElementById("scrollToTop");
 
-// Show the button when the user scrolls down 100px
-window.onscroll = function () {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    scrollToTopButton.style.display = "block";
-  } else {
-    scrollToTopButton.style.display = "none";
-  }
-};
-
-// Scroll to the top when the button is clicked
-scrollToTopButton.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth", // Smooth scrolling
-  });
-});
 export default store;
