468ea30120b44fadf5c5e6f32e1489da2015b4e4
soham247 <sohamsadhukhan247@gmail.com>
2025-01-10 12:39:07 +0530

    added contributors on homepage

--- src/components/contributors/contributorsLink.jsx
+++ src/components/contributors/contributorsLink.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { getContributors } from "./contribution.js";
 import { Link } from 'react-router-dom';
 
-function ContributorsLink() {
+function ContributorsLink({w=10}) {
     const [data, setData] = useState([]);
     
     const getData = async () => {
@@ -17,27 +17,29 @@ function ContributorsLink() {
     }, []);
 
     return (
-        <Link to="/contributors">
-            {data ? (
-                <div className="flex items-center">
-                    <div className="flex -space-x-3">
-                        {data.map((item) => (
-                            <img 
-                                src={item.avatar_url} 
-                                key={item.id} 
-                                className="w-10 h-10 rounded-full border-2 border-white hover:scale-105 transition-transform duration-200 ease-in-out"
-                                alt={`${item.login}'s avatar`}
-                            />
-                        ))}
+        <div className='w-full flex justify-center'>
+            <Link to="/contributors">
+                {data ? (
+                    <div className="flex items-center">
+                        <div className="flex -space-x-3">
+                            {data.map((item) => (
+                                <img 
+                                    src={item.avatar_url} 
+                                    key={item.id} 
+                                    className={`w-${w} h-${w} rounded-full border-2 border-white hover:scale-105 transition-transform duration-200 ease-in-out`}
+                                    alt={`${item.login}'s avatar`}
+                                />
+                            ))}
+                        </div>
+                        <span className="text-blue-600 hover:text-blue-800 font-medium">
+                            +more
+                        </span>
                     </div>
-                    <span className="text-blue-600 hover:text-blue-800 font-medium">
-                        +more
-                    </span>
-                </div>
-            ) : (
-                <p className="font-semibold my-5">Contributors</p>
-            )}
-        </Link>
+                ) : (
+                    <p className="font-semibold my-5">Contributors</p>
+                )}
+            </Link>
+        </div>
     )
 }
 
