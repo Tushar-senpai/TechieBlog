8c04071dd03c22ba72a4ba14bec3f153ab9da686
DIVYAM VERMA <divyamverma90@gmail.com>
2025-06-18 17:10:18 +0530

    Refactor Login and Signup components for improved readability and added password visibility toggle

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,14 +5,16 @@ import authService from "../appwrite/auth";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import Swal from "sweetalert2";
-import image from '../../src/assets/Computer.svg';
+import image from "../../src/assets/Computer.svg";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import { Eye, EyeOff } from "lucide-react";
 
 function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { register, handleSubmit } = useForm();
+  const [showPassword, setShowPassword] = useState(false);
 
   const login = async (data) => {
     try {
@@ -51,17 +53,21 @@ function Login() {
     <div className="font-[sans-serif]">
       <div className="min-h-screen flex flex-col items-center justify-center py-2 px-4">
         <div className="grid md:grid-cols-2 items-center gap-2 max-w-6xl w-full">
-          <motion.div 
-            initial={{ opacity: 0, x: -100 }} 
-            animate={{ opacity: 1, x: 0 }} 
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 1 }}
             className="max-md:mt-8"
           >
-            <img src={image} className="w-full aspect-[71/50] max-md:w-4/5 mx-auto block object-cover" alt="Dining Experience" />
+            <img
+              src={image}
+              className="w-full aspect-[71/50] max-md:w-4/5 mx-auto block object-cover"
+              alt="Dining Experience"
+            />
           </motion.div>
-          <motion.div 
-            initial={{ opacity: 0, x: 100 }} 
-            animate={{ opacity: 1, x: 0 }} 
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 1 }}
             className="border border-gray-300 rounded-lg p-6 max-w-md shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] max-md:mx-auto"
           >
@@ -73,49 +79,83 @@ function Login() {
               </div>
               <div className="mb-8">
                 <h3 className="text-orange-600 text-3xl font-bold">Sign in</h3>
-                <p className="text-gray-500 text-sm mt-4 leading-relaxed dark:text-white">Sign in to your account and explore a world of possibilities. Your journey begins here.</p>
+                <p className="text-gray-500 text-sm mt-4 leading-relaxed dark:text-white">
+                  Sign in to your account and explore a world of possibilities.
+                  Your journey begins here.
+                </p>
               </div>
 
               <div>
-                <label className="text-gray-800 text-sm mb-2 block dark:text-white">Email</label>
+                <label className="text-gray-800 text-sm mb-2 block dark:text-white">
+                  Email
+                </label>
                 <div className="relative flex items-center">
-                  <Input name="username" type="email" required className="hover-input w-full text-sm text-gray-800 border border-gray-300 pl-4 pr-10 py-3 rounded-lg outline-orange-600" placeholder="Enter email" 
+                  <Input
+                    name="username"
+                    type="email"
+                    required
+                    className="hover-input w-full text-sm text-gray-800 border border-gray-300 pl-4 pr-10 py-3 rounded-lg outline-orange-600"
+                    placeholder="Enter email"
                     {...register("email", {
                       required: true,
                       validate: {
                         matchPattern: (value) =>
-                          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                          "Email address must be a valid address",
+                          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+                            value
+                          ) || "Email address must be a valid address",
                       },
                     })}
                   />
                 </div>
               </div>
               <div>
-                <label className="text-gray-800 text-sm mb-2 block dark:text-white">Password</label>
-                <div className="relative flex items-center">
-                  <Input name="password" type="password" required className="hover-input w-full text-sm text-gray-800 border border-gray-300 pl-4 pr-10 py-3 rounded-lg outline-orange-600" placeholder="Enter password" 
-                    {...register("password", {
-                      required: true,
-                    })}
+                <label className="text-gray-800 text-sm mb-2 block dark:text-white">
+                  Password
+                </label>
+                <div className="relative">
+                  <Input
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    required
+                    {...register("password", { required: true })}
+                    className="w-full text-sm text-gray-800 border border-gray-300 p-3 pr-10 rounded-lg outline-orange-600"
+                    placeholder="Enter password"
                   />
+                  <span
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-600"
+                  >
+                    {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                  </span>
                 </div>
               </div>
 
               <div className="flex flex-wrap items-center justify-between gap-4">
                 <div className="text-sm">
-                  <Link to="/forgot-password" className="text-sm text-primary dark:text-blue-400 hover:underline">Forgot Your Password?</Link>
+                  <Link
+                    to="/forgot-password"
+                    className="text-sm text-primary dark:text-blue-400 hover:underline"
+                  >
+                    Forgot Your Password?
+                  </Link>
                 </div>
               </div>
 
               <div className="!mt-8">
-                <Button type="submit" className="w-full bg-orange-600 dark:bg-blue-500 text-white hover:opacity-90 hover:shadow-lg active:scale-95 transition-all dark:shadow-blue-500/50 shadow-xl py-2.5 px-4 text-sm tracking-wide rounded-lg hover:bg-blue-700 focus:outline-none">
+                <Button
+                  type="submit"
+                  className="w-full bg-orange-600 dark:bg-blue-500 text-white hover:opacity-90 hover:shadow-lg active:scale-95 transition-all dark:shadow-blue-500/50 shadow-xl py-2.5 px-4 text-sm tracking-wide rounded-lg hover:bg-blue-700 focus:outline-none"
+                >
                   Sign in
                 </Button>
               </div>
 
-              <p className="text-sm !mt-8 text-center text-gray-500 dark:text-white">Don't have an account?               
-                <Link to="/signup" className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap dark:text-blue-400 transition-all duration-200">
+              <p className="text-sm !mt-8 text-center text-gray-500 dark:text-white">
+                Don't have an account?
+                <Link
+                  to="/signup"
+                  className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap dark:text-blue-400 transition-all duration-200"
+                >
                   Register here
                 </Link>
               </p>
